327aaf92a26c4d857715ff498238a7eb999e1ae1
Matthew <matthew25kim@gmail.com>
2019-12-25 00:02:23 -0500

    update

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function processPostback(event) {
                 greeting = "Hi " + name + "! ";
             }
             var firstMessage = greeting + "Thanks for joining RCF Meets!";
-            var url = "https:\/\/www.facebook.com\/pennrcf\/photos\/a.318849449044296\/422291922033381\/"
+            var url = "https://www.facebook.com/pennrcf/photos/a.318849449044296/422291922033381/"
             var secondMessage = "To begin, let's build your profile! What's something you like to do in your free time?" + 
             " No need to write an essay - a couple interests should do."
             sendAttachment(senderId, url);
