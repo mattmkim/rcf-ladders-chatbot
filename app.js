3e58b48f249b537217fb9f709ea68c2ce494241d
Matthew <matthew25kim@gmail.com>
2019-12-25 23:21:08 -0500

    update

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function processPostback(event) {
                 } else {
                     var result = response[0];
                     result = result._id.toString();
-                    console.log(JSON.parse(result));
+                    console.log(result);
                     if (JSON.parse(response[0]).interests) {
                         sendMessage(senderId, {text: firstMessage});
                         sendMessage(senderId, {text: secondMessage});
