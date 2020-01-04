b7787d9f9b79ece568fac63e08168103982142ee
Matthew <matthew25kim@gmail.com>
2020-01-04 17:59:52 -0500

    update

--- models/users.js
+++ models/users.js
@@ -8,7 +8,6 @@ var UserSchema = new Schema({
   firstName: {type: String},
   lastName: {type: String},
   profileUrl: {type: String},
-  userLink: {type: String},
   available: {type: Boolean}
 });
 
