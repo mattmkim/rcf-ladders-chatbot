80497b899b64cb78875b3563c3a30a95594f8509
Matthew <matthew25kim@gmail.com>
2020-01-17 15:45:22 -0500

    update

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get('/options', (req, res, next) => {
         } else if (referer.indexOf('www.facebook.com') >= 0) {
             res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
         }
-        res.sendFile('public/options.html', {root: __dirname});
+        res.sendFile('webview.html', {root: __dirname});
     }
 });
 
