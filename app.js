2d108ff0562e4c13426571bc3fdd5bc66ad9766b
Matthew <matthew25kim@gmail.com>
2020-01-18 18:33:14 -0500

    update

--- app.js
+++ app.js
@@ -96,6 +96,19 @@ app.get('/preferences', (req, res, next) => {
     }
 });
 
+app.get('/preferencesspostback', (req, res) => {
+    let body = req.query;
+    var newMessage = "Great, you're all signed up! Keep on the lookout for weekly messages from us on Sundays!";
+    var viewMembersMessage = "In the meantime, type " + '"' + "View Members" + '"' + " if you would like to get a preview of who else is in RCF Meets!";
+
+    console.log(body);
+
+    // code to update status of user (list of people to not pair up with)
+    // need to somehow send back user id
+    res.status(200).send('Please close this window to return to the conversation thread.');
+    //sendTwoMessages(senderId, newMessage, viewMembersMessage);
+});
+
 
 // Facebook Webhook
 // Used for verification
