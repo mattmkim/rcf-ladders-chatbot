deb70f5aaa4f83e44bada9a1250a1c630ab74f42
Matthew <matthew25kim@gmail.com>
2020-01-18 15:53:04 -0500

    update

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.listen((process.env.PORT || 5000));
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 // Server index page
 app.get("/", function (req, res) {
