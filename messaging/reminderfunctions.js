2b58b56a8b51c697d75f9d13ea583cccec8020a8
Matthew <matthew25kim@gmail.com>
2020-05-01 17:52:19 -0400

    update

--- messaging/reminderfunctions.js
+++ messaging/reminderfunctions.js
@@ -4,8 +4,12 @@ module.exports = function(User) {
     var module = {}
 
     module.sendInfo = function() {
-        var message = "Sorry, we meant 10 am EDT!";
-        User.find({}, function(err, response) {
+        var message = "Hello! As CGs and this semester comes to a close, RCF Meets will also no longer be running for at least a couple of weeks - " +
+        "however, we’ll definitely bring this back online sometime over the summer! Be prepared to make a new account - we will most likely be deleting all users from the database." + 
+        " Hope that this has helped us stay connected as a community and has been a good experience for you all! We will continue to work on additional features and prepare to welcome" + 
+        " in freshman and newcomers for this upcoming year :) \n\n" +
+        "Let’s continue to make the effort to stay connected and cover each other in prayer, and finish the semester strong! Almost there y’all! " + '"' +  "And let us consider how to stir up one another to love and good works, not neglecting to meet together, as is the habit of some, but encouraging one another, and all the more as you see the Day drawing near." + "'" +  " (Hebrews 10:24-25)";
+        User.find({user_id: "2479283145514220"}, function(err, response) {
             if (err) {
                 console.log(err);
             } else {
