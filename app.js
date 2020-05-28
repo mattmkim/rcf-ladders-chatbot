6519781fea0af21378029c66454a8531fb1420e2
Matthew <matthew25kim@gmail.com>
2020-05-27 21:19:07 -0400

    update path

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ var webhookRoutes = require('./routes/webhookroutes.js')(User);
 var userFunctions = require('./backend/userfunctions')(User);
 var reminderFunctions = require('./messaging/reminderfunctions')(User);
 
-
-app.use("/public", express.static(__dirname + '/public'));
-app.use("/", express.static(__dirname + '/client/build/static'));
+app.use("/public", express.static(path.join(__dirname, "public")))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
