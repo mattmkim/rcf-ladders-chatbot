cb2cc6e6c9748aa16cd542fa2bbc6daf1e2eaeb3
Matthew <matthew25kim@gmail.com>
2020-05-27 21:23:04 -0400

    new build

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var userFunctions = require('./backend/userfunctions')(User);
 var reminderFunctions = require('./messaging/reminderfunctions')(User);
 
 app.use("/public", express.static(path.join(__dirname, "public")))
+app.use("/", express.static(path.join(__dirname, "public", "build")))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
