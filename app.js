80c3660c93517c4ac6b38c8d20f09177ff64925f
Matthew <matthew25kim@gmail.com>
2020-06-03 20:23:07 -0400

    serialize and deserialize functions

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ passport.use(new FacebookStrategy({
     clientID: "429499001267322",
     clientSecret: "bffab64e1317a9e89619a5532d78f9ab",
     callbackURL: "https://rcf-meets.herokuapp.com/auth/facebook/callback",
-    profileFields: ['id', 'profile_pic', 'name', 'displayName']
+    profileFields: ['id', 'picture.type(large)', 'displayName']
 },
 function(accessToken, refreshToken, profile, done) {
     console.log(profile);
