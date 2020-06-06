1f7cc73623d81eece5cbab4599fc1bd5d1273e5c
Matthew <matthew25kim@gmail.com>
2020-06-06 16:28:00 -0400

    added checks before sending photos

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
+import {Card, Navbar} from 'react-bootstrap'
+import '../public/style/Feed.css'
 
 class Feed extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
+
+    async componentDidMount() {
+        
+    }
+
     render() {
         return (
             <div>
-                <h1>welcome to rcf meets ahahahahah</h1>
+                <Navbar className="navbar-feed" bg="white">
+                    <div class="title-feed">
+                        RCFgram
+                    </div>
+                </Navbar>
+                
             </div>
         )
     }
