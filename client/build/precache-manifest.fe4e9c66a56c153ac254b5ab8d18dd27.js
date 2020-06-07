794bb4daef5a6108b93fa84cacd8a5fe4f2d14d7
Matthew <matthew25kim@gmail.com>
2020-06-06 23:31:03 -0400

    final build

---
+++ client/build/precache-manifest.fe4e9c66a56c153ac254b5ab8d18dd27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747bfb1fdf882ca07a4391e81e83cefe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01123ad7efecf0319e62",
+    "url": "/static/css/main.b821619c.chunk.css"
+  },
+  {
+    "revision": "5292020ed243abccf937",
+    "url": "/static/js/2.7910923f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7910923f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01123ad7efecf0319e62",
+    "url": "/static/js/main.91f82bd4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "0a98477abd2274e176c1760a5d5d3e66",
+    "url": "/static/media/rcfmeets.0a98477a.png"
+  }
+]);
