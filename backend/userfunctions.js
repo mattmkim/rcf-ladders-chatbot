5e76126cd360f967a9793d1430424e493fa6370f
Matthew <matthew25kim@gmail.com>
2020-10-04 00:17:23 -0400

    update QOW

--- backend/userfunctions.js
+++ backend/userfunctions.js
@@ -243,7 +243,7 @@ module.exports = function(User) {
                             })
                             console.log(f.firstName + f.lastName + " matched with " + s.firstName + s.lastName + " and " + t.firstName + t.lastName);
                             var questionOfWeek = 
-                            "Here is the Question of the Week:\nWhat's your Meyers Briggs?\n\n" +
+                            "Here is the Question of the Week:\nWhat love language do you like to receive/give?\n\n" +
                             "And this week's Challenge of the Week:\nTake a funny picture with your partner!";
 
                             msg.sendMessage(f.user_id, {text: questionOfWeek});
@@ -284,7 +284,7 @@ module.exports = function(User) {
                         } else {
                             console.log(f.firstName + f.lastName + " matched with " + s.firstName + s.lastName);
                             var questionOfWeek = 
-                            "Here is the Question of the Week:\nWhat's your Meyers Briggs?\n\n" +
+                            "Here is the Question of the Week:\nWhat love language do you like to receive/give?\n\n" +
                             "And this week's Challenge of the Week:\nTake a funny picture with your partner!";
 
                             msg.sendMessage(f.user_id, {text: questionOfWeek});
